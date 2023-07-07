8ba8170a9f5e3346032f793430ab9733c7b2baad
ANSHU SINGH <121624962+anshusingh15@users.noreply.github.com>
2023-07-07 22:29:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+var randomNumber1 =Math.floor(Math.random()*6)+1;
+var randomIamge="dice"+randomNumber1+".png";
+var randomImageSource="./images/"+randomIamge;
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSource);
+
+
+
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var randomImage2="dice"+randomNumber2+".png";
+var randomImageSource2="./images/"+randomImage2;
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSource2);
+
+if(randomNumber1===randomNumber2){
+    document.querySelector("h1").innerHTML="Draw!!";
+}
+
+else if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 wins🚩";
+}
+else if(randomNumber1< randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 wins🚩";
+}
